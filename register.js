ed85756f303da7fe753bed019ab3f42df26aea25
Topik Mujianto <topik.mujianto@finaccel.co>
2024-05-18 04:00:43 +0700

    add console log test

--- register.js
+++ register.js
@@ -2,4 +2,5 @@ function login(username, password, tanggal_lahir) {
     console.log('username : ', username)
     console.log('password : ', password)
     console.log('tanggal_lahir : ', tanggal_lahir)
+    console.log('test')
 }
