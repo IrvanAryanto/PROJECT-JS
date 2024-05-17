7f60d25442fa884b55f3378d0e60e2fbe323acf5
Topik Mujianto <topik.mujianto@finaccel.co>
2024-05-18 03:43:16 +0700

    add nik as a param

--- register.js
+++ register.js
@@ -1,5 +1,6 @@
-function login(username, password, tanggal_lahir) {    
+function login(username, password, tanggal_lahir, nik) {    
     console.log('username : ', username)
     console.log('password : ', password)
     console.log('tanggal_lahir : ', tanggal_lahir)
+    console.log('NIK : ', nik)
 }
