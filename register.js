76ce74f0f54bcd01c36822514ecb640496eb9e18
IrvanAryanto <irvan123n@gmail.com>
2024-05-18 03:13:51 +0700

    register

---
+++ register.js
@@ -0,0 +1,5 @@
+function login(username, password, tanggal_lahir) {    
+    console.log('username : ', username)
+    console.log('password : ', password)
+    console.log('tanggal_lahir : ', tanggal_lahir)
+}
